16e431ed486a66d73bcd764bd94537a6f9f143b2
cankush625 <cankush625@gmail.com>
2019-11-15 18:20:15 +0530

    Basic UI created
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- sample/Main.java
+++ sample/Main.java
@@ -1,25 +1,34 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
         //Manual code for replacing the starting code in sample.fxml file
-        GridPane root = new GridPane();
-        root.setAlignment(Pos.CENTER);
-        root.setVgap(10);
-        root.setHgap(10);
+//        GridPane root = new GridPane();
+//        root.setAlignment(Pos.CENTER); //Aligning to the centre
+//        root.setVgap(10); //Vertical gap
+//        root.setHgap(10); //Horizontal gap
 
-        primaryStage.setTitle("Hello JavaFX!");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Hello JavaFX!"); //Title for the application to display in the title bar
+        primaryStage.setScene(new Scene(root, 300, 275)); //Setting height and width for the application screen
+
+        //Adding label for welcoming to the application
+//        Label greeting = new Label("Welcome to JavaFX!");
+//        greeting.setTextFill(Color.GREEN); //Setting the color of the text to green
+//        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 40)); //Setting the font properties
+//
+//        root.getChildren().add(greeting); //Adding controller
+
+        //Showing the application
         primaryStage.show();
     }
 
@@ -28,3 +37,5 @@ public class Main extends Application {
         launch(args);
     }
 }
+
+//by Ankush Chavan
