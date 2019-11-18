cc4bf022642ece478b46c9381843cdb236f2d86f
Ankush Chavan <41515472+cankush625@users.noreply.github.com>
2019-11-18 18:01:14 +0530

    BorderPane, FlowPane, TilePane and StackPane are added (#2)
    * Borderpane code is added
    
    Signed-off-by: cankush625 <cankush625@gmail.com>
    
    * FlowPane, TilePane and StackPane layout is added
    
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ layouts/borderpane_layout/Main.java
@@ -0,0 +1,26 @@
+package borderpane_layout;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        primaryStage.setTitle("Hello JavaFX!"); //Title for the application to display in the title bar
+        primaryStage.setScene(new Scene(root, 500, 275)); //Setting height and width for the application screen
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+//by Ankush Chavan
