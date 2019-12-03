4702581c8126ce8fa560083c039708f79f6a894d
Ankush Chavan <41515472+cankush625@users.noreply.github.com>
2019-12-03 19:33:34 +0530

    Thread and Runnable implemented (#8)
    * Thread and Runnable implemented
    
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- events and event handlers/events_and_event_handlers/Controller.java
+++ events and event handlers/events_and_event_handlers/Controller.java
@@ -1,5 +1,14 @@
 package events_and_event_handlers;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import javafx.event.ActionEvent;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -12,6 +21,10 @@ public class Controller {
     private Button helloButton;
     @FXML
     private Button byeButton;
+    @FXML
+    private CheckBox ourCheckBox;
+    @FXML
+    private Label ourLabel;
 
     /**
      *
@@ -32,12 +45,47 @@ public class Controller {
 //        System.out.println("Following button was pressed: " + e.getSource());
 //    }
 
+    /**
+     *
+     * @param e getting the ActionEvent
+     */
     @FXML
     public void onButtonClicked(ActionEvent e) {
         if (e.getSource().equals(helloButton)) {
             System.out.println("Hello, " + nameField.getText());
         } else if (e.getSource().equals(byeButton)) {
             System.out.println("Bye, " + nameField.getText());
+        }
+
+        // If we make the thread to sleep directly it will halt the whole program for that sleep time
+        // The better way to handle this is running the thread in the background
+        // Setting up the process to run in the background by implementing runnable
+        Runnable task = new Runnable() {
+            @Override
+            public void run() { // This is running in background thread, so unable to update label
+                try{
+                    Thread.sleep(5000);
+                    // To get rid of the Exception lets run the label updating after process is get completing. So that is label is
+                    // getting updated on the UI Thread and we don't get any exception
+                    Platform.runLater(new Runnable() { // Opening new Runnable to update the label
+                        @Override
+                        public void run() { // This is running in the UI Thread so updating the label
+                            ourLabel.setText("We did something");
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    // We don't care about this
+                }
+            }
+        };
+
+        // Starting the above task
+        new Thread(task).start();
+
+        if (ourCheckBox.isSelected()) {
+            nameField.clear();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
         }
     }
 
@@ -51,4 +99,11 @@ public class Controller {
         helloButton.setDisable(disableButtons);
         byeButton.setDisable(disableButtons);
     }
+
+    @FXML
+    public void handleChange() {
+        System.out.println("The checkbox is " + (ourCheckBox.isSelected() ? "checked" : "not checked"));
+    }
 }
+
+//by Ankush Chavan
